7cd710318061bad488e641809a60b878526c3b81
heritagearchitecture <heritagearchitecture218@gmail.com>
2025-12-28 05:12:02 +0000

    Add ambient module declarations for '@' path aliases to help editor resolve imports

---
+++ src/types/aliases.d.ts
@@ -0,0 +1,5 @@
+// Ambient module declarations to help the editor resolve path aliases
+declare module "@/components/*";
+declare module "@/components/ui/*";
+declare module "@/hooks/*";
+declare module "@/*";
